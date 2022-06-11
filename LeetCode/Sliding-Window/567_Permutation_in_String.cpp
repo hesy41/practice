59dd5447ebc97477c525af8efed6a8c2c2b5f910
hesy41 <janethsy0806@gmail.com>
2022-06-11 17:31:23 -0400

    leetcode 567
    Signed-off-by: hesy41 <janethsy0806@gmail.com>

--- LeetCode/Sliding-Window/567_Permutation_in_String.cpp
+++ LeetCode/Sliding-Window/567_Permutation_in_String.cpp
@@ -35,3 +35,67 @@ public:
         return false;
     }
 };
+
+//shorter version
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> s1_freq, s2_window;
+        if (s1.size()>s2.size())
+            return false; 
+        
+        for (int a=0; a<s1.size(); a++)
+        {
+            s1_freq[s1[a]]++;
+            s2_window[s2[a]]++;
+            //ini the window to be the same size as s1
+        }
+        if(s1_freq == s2_window)
+                return true;
+        int i=0;
+        int j=s1.size();
+        while (j<s2.size())
+        {
+            if (s2_window[s2[i]] == 1)
+                s2_window.erase(s2[i++]);
+            else
+                s2_window[s2[i++]]-=1;
+            
+            s2_window[s2[j++]]+=1;
+            
+            if(s1_freq == s2_window)
+                return true;
+        }
+        return false;
+    }
+};
+
+//solution 2 with vector and no coping the window at first
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
+        vector<int> s1v (26, 0);
+        vector<int> s2v (26, 0);
+        for (auto c : s1)
+            s1v[c - 'a']++;
+        
+        int l = 0, r = 0;
+        
+        while (r < s2.size()) {
+            s2v[s2[r]-'a']++;
+            if (r - l + 1 == s1.size() && s1v == s2v)
+                return true;
+            
+            if (r - l + 1 < s1.size()) 
+                r++;
+            else 
+            {
+                s2v[s2[l]-'a']--;
+                l++;
+                r++;
+            }
+        }
+        return false;
+    }
+};

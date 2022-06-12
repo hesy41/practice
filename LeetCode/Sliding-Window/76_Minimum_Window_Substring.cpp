31d8ff31ffc43d9be8171f02178b84d9a301bdfc
hesy41 <janethsy0806@gmail.com>
2022-06-12 17:49:52 -0400

    leetcode 76
    Signed-off-by: hesy41 <janethsy0806@gmail.com>

---
+++ LeetCode/Sliding-Window/76_Minimum_Window_Substring.cpp
@@ -0,0 +1,50 @@
+/*76. Minimum Window Substring
+  Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+A substring is a contiguous sequence of characters within the string.*/
+
+//using unordered_map
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        string ans="";
+        if (s.length() < t.length())
+            return ans;
+        if(s==t)
+            return s;
+        
+        unordered_map<char, int> rec;
+        for (auto h : t)
+            rec[h]++;
+        
+        int i=0, j=0, counter = t.size(), minStart=0, minLen=INT_MAX;
+        
+        while(j< s.size())
+        {
+            rec[s[j]]--;
+            
+            if (rec[s[j]] >= 0) // s[j] is in t
+                counter--;
+            
+            while(counter ==0) //substring is found
+            {
+                if(j-i+1 < minLen)
+                {
+                    minStart = i;
+                    minLen= j-i+1;
+                }
+                
+                //move i to see whether there is a storter substring
+                rec[s[i]]++;
+                if(rec[s[i]] > 0)
+                    counter++;
+                i++; 
+            }
+            j++;
+        }
+        
+        return minLen == INT_MAX ? "": s.substr(minStart, minLen);
+    }
+};

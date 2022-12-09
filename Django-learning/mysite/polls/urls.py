8f82cddcce1aea34b68e490fe3c71c4da6b3138e
hesy41 <janethsy0806@gmail.com>
2022-12-08 23:33:11 -0800

    finish django tutorial
    Signed-off-by: hesy41 <janethsy0806@gmail.com>

--- Django-learning/mysite/polls/urls.py
+++ Django-learning/mysite/polls/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 # url config that map view.py to a url so that we could call it
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]

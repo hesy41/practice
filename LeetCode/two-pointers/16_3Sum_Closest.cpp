1d1752b65993755b98ab05b2ddf6bd1c232e2102
hesy41 <janethsy0806@gmail.com>
2023-01-06 14:36:55 -0800

    LC 16
    Signed-off-by: hesy41 <janethsy0806@gmail.com>

--- LeetCode/two-pointers/16_3Sum_Closest.cpp
+++ LeetCode/two-pointers/16_3Sum_Closest.cpp
@@ -35,3 +35,30 @@ public:
         return ans;
     }
 };
+
+// attempt Jan 6 2023
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int ans = nums[0]+nums[1]+nums[2];
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i=0; i<n-2; i++)
+        {
+            int lo = i+1, hi = n-1;
+            while(lo < hi)
+            {
+                int sum = nums[i]+nums[lo]+nums[hi];
+                if (sum == target)
+                    return sum;
+                if (abs(sum - target) < abs(ans - target))
+                    ans = sum;
+                if (sum > target)
+                    --hi;
+                else
+                    ++lo;                
+            }
+        }
+        return ans;
+    }
+};

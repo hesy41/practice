7bd80937f0bb8aa56c1360838f53b3778bf33719
hesy41 <janethsy0806@gmail.com>
2023-01-19 08:56:07 -0800

    LC 121
    Signed-off-by: hesy41 <janethsy0806@gmail.com>

---
+++ LeetCode/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,24 @@
+/*121. Best Time to Buy and Sell Stock
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int slow =0, fast =1;
+        int max=0;
+        while(fast < prices.size())
+        {
+            int local = prices[fast] - prices[slow];
+            max = local > max ? local:max;
+            if(prices[slow]>prices[fast])
+                slow = fast;
+            fast ++;
+        }
+        return max;
+    }
+};
